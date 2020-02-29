f94073dc257f680b269c1bfa75d247085941eba5
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-02-29 16:10:35 -0500

    setup server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs = require("express-handlebars");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, function(){
+    console.log("http://localhost:"+ PORT);
+});
